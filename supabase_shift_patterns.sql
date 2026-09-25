7b661ed4c45575661c13fb60c16c7b6c0b8cdf2c
mgocalpissodaa-code <mgocalpissodaa@gmail.com>
2026-09-25 16:21:11 +0900

    鎌田の勤務時間SQLを7時-6時に修正

--- supabase_shift_patterns.sql
+++ supabase_shift_patterns.sql
@@ -65,7 +65,7 @@ begin
     end if;
 
   elsif v_name = '鎌田フサ子' then
-    if p_shift_pattern not in ('22:00-06:00', '19:00-06:00', '07:00-10:00') then
+    if p_shift_pattern not in ('22:00-06:00', '07:00-06:00', '07:00-10:00') then
       raise exception '鎌田さんの勤務時間を選び直してください。';
     end if;
 
